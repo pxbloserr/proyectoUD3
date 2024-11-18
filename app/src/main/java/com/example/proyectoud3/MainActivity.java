a641ccb0e06938bb3be5c073a1e6f9aca0f1b268
pxbloserr <pabloserrperez@gmail.com>
2024-11-18 20:36:19 +0100

    Se arreglaron margenes

--- app/src/main/java/com/example/proyectoud3/MainActivity.java
+++ app/src/main/java/com/example/proyectoud3/MainActivity.java
@@ -36,38 +36,7 @@ public class MainActivity extends AppCompatActivity implements AdaptadorProducto
             return insets;
         });
 
-        productos = new ArrayList<>(Arrays.asList(new Producto[]{
-                new Producto("Pan de molde", 1.50, R.drawable.pan_de_molde, 15),
-                new Producto("Leche", 0.99, R.drawable.leche, 20),
-                new Producto("Huevos", 2.30, R.drawable.huevos, 0),
-                new Producto("Arroz", 1.10, R.drawable.arroz, 12),
-                new Producto("Pasta", 1.20, R.drawable.pasta, 8),
-                new Producto("Harina", 0.85, R.drawable.harina, 0),
-                new Producto("Azúcar", 1.00, R.drawable.azucar, 25),
-                new Producto("Sal", 0.60, R.drawable.sal, 18),
-                new Producto("Aceite de oliva", 4.50, R.drawable.aceite_de_oliva, 5),
-                new Producto("Aceite de girasol", 3.20, R.drawable.aceite_de_girasol, 10),
-                new Producto("Café", 2.50, R.drawable.cafe, 7),
-                new Producto("Té", 1.80, R.drawable.te, 12),
-                new Producto("Agua embotellada", 0.50, R.drawable.agua_embotellada, 30),
-                new Producto("Refrescos", 1.20, R.drawable.refrescos, 0),
-                new Producto("Jugo natural", 1.50, R.drawable.jugo_natural, 8),
-                new Producto("Mantequilla", 1.70, R.drawable.mantequilla, 10),
-                new Producto("Queso", 2.80, R.drawable.queso, 5),
-                new Producto("Yogur", 0.90, R.drawable.yogur, 20),
-                new Producto("Carne", 5.50, R.drawable.carne, 3),
-                new Producto("Pescado", 6.00, R.drawable.pescado, 2),
-                new Producto("Manzanas", 1.20, R.drawable.manzanas, 15),
-                new Producto("Zanahorias", 0.80, R.drawable.zanahorias, 18),
-                new Producto("Patatas", 0.70, R.drawable.patatas, 22),
-                new Producto("Cereal", 2.90, R.drawable.cereal, 7),
-                new Producto("Lentejas", 1.50, R.drawable.lentejas, 10),
-                new Producto("Detergente", 4.00, R.drawable.detergente, 5),
-                new Producto("Papel higiénico", 3.20, R.drawable.papel_higienico, 12),
-                new Producto("Pasta de dientes", 2.00, R.drawable.pasta_de_dientes, 0),
-                new Producto("Jabón líquido", 3.50, R.drawable.jabon_liquido, 8),
-                new Producto("Papel de cocina", 1.80, R.drawable.papel_de_cocina, 0)
-        }));
+        productos = cargarProductos ();
 
         ocultarSwitch();
 
@@ -209,6 +178,45 @@ public class MainActivity extends AppCompatActivity implements AdaptadorProducto
         RecyclerView rvCatalogo = findViewById(R.id.rvCatalogo);
         rvCatalogo.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
         rvCatalogo.setAdapter(adaptadorCesta);
+
+    }
+
+    private ArrayList <Producto> cargarProductos () {
+
+        ArrayList <Producto> catalogo = new ArrayList<>(Arrays.asList(new Producto[]{
+                new Producto("Pan de molde", 1.50, R.drawable.pan_de_molde, 15),
+                new Producto("Leche", 0.99, R.drawable.leche, 20),
+                new Producto("Huevos", 2.30, R.drawable.huevos, 0),
+                new Producto("Arroz", 1.10, R.drawable.arroz, 12),
+                new Producto("Pasta", 1.20, R.drawable.pasta, 8),
+                new Producto("Harina", 0.85, R.drawable.harina, 0),
+                new Producto("Azúcar", 1.00, R.drawable.azucar, 25),
+                new Producto("Sal", 0.60, R.drawable.sal, 18),
+                new Producto("Aceite de oliva", 4.50, R.drawable.aceite_de_oliva, 5),
+                new Producto("Aceite de girasol", 3.20, R.drawable.aceite_de_girasol, 10),
+                new Producto("Café", 2.50, R.drawable.cafe, 7),
+                new Producto("Té", 1.80, R.drawable.te, 12),
+                new Producto("Agua embotellada", 0.50, R.drawable.agua_embotellada, 30),
+                new Producto("Refrescos", 1.20, R.drawable.refrescos, 0),
+                new Producto("Jugo natural", 1.50, R.drawable.jugo_natural, 8),
+                new Producto("Mantequilla", 1.70, R.drawable.mantequilla, 10),
+                new Producto("Queso", 2.80, R.drawable.queso, 5),
+                new Producto("Yogur", 0.90, R.drawable.yogur, 20),
+                new Producto("Carne", 5.50, R.drawable.carne, 3),
+                new Producto("Pescado", 6.00, R.drawable.pescado, 2),
+                new Producto("Manzanas", 1.20, R.drawable.manzanas, 15),
+                new Producto("Zanahorias", 0.80, R.drawable.zanahorias, 18),
+                new Producto("Patatas", 0.70, R.drawable.patatas, 22),
+                new Producto("Cereal", 2.90, R.drawable.cereal, 7),
+                new Producto("Lentejas", 1.50, R.drawable.lentejas, 10),
+                new Producto("Detergente", 4.00, R.drawable.detergente, 5),
+                new Producto("Papel higiénico", 3.20, R.drawable.papel_higienico, 12),
+                new Producto("Pasta de dientes", 2.00, R.drawable.pasta_de_dientes, 0),
+                new Producto("Jabón líquido", 3.50, R.drawable.jabon_liquido, 8),
+                new Producto("Papel de cocina", 1.80, R.drawable.papel_de_cocina, 0)
+        }));
+
+        return catalogo;
 
     }
 
